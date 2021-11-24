4ec95eba8864eff30d4fcf7ac874c2dab5266e45
Alexander <sashashvetsov3@gmail.com>
2021-11-24 21:22:28 +0300

    исправил табы, теперь работают

--- script/script.js
+++ script/script.js
@@ -5,14 +5,18 @@ let infoTab = document.querySelectorAll('.info-tabs__content')
 for (let i = 0; i < itemTab.length; i++) {
    itemTab[i].addEventListener('click', (e) => {
       e.preventDefault()
+      let tabTag = e.target.getAttribute("item-tab")
 
-      itemTab[0].classList.remove('info-tabs__item_active')
-      itemTab[1].classList.remove('info-tabs__item_active')
-      itemTab[2].classList.remove('info-tabs__item_active')
-      e.target.classList.add('info-tabs__item_active')
-
-      for (let i = 0; i < infoTab.length; i++) {
-         infoTab[i].classList.remove('info-tabs__content_active')
+      for (let j = 0; j < infoTab.length; j++) {
+         let infoTag = infoTab[j].getAttribute("info-tab")
+         
+         if (tabTag === infoTag) {
+            itemTab[j].classList.add("info-tabs__item_active")
+            infoTab[j].classList.add("info-tabs__content_active")
+         } else {
+            itemTab[j].classList.remove("info-tabs__item_active")
+            infoTab[j].classList.remove("info-tabs__content_active")
+         }
       }
    })
 }
