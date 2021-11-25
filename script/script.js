95b4065d4a6d4cc4ef057f33705fb7d79e0784d0
Alexander <sashashvetsov3@gmail.com>
2021-11-25 21:50:05 +0300

    реализация слайдера только вправо

--- script/script.js
+++ script/script.js
@@ -1,3 +1,4 @@
+// !-------------------------табы----------------------------
 let itemTab = document.querySelectorAll('.info-tabs__item')
 let infoTab = document.querySelectorAll('.info-tabs__content')
 
@@ -9,7 +10,7 @@ for (let i = 0; i < itemTab.length; i++) {
 
       for (let j = 0; j < infoTab.length; j++) {
          let infoTag = infoTab[j].getAttribute("info-tab")
-         
+
          if (tabTag === infoTag) {
             itemTab[j].classList.add("info-tabs__item_active")
             infoTab[j].classList.add("info-tabs__content_active")
@@ -20,6 +21,27 @@ for (let i = 0; i < itemTab.length; i++) {
       }
    })
 }
+
+// !-------------------------слайдер----------------------------
+let btnR = document.querySelector('.buttonSlider-right')
+let sliderItem = document.querySelectorAll('.info-slider__item')
+let i = 0
+
+console.log(btnR);
+
+btnR.addEventListener('click', () => {
+   i++
+   if (i >= sliderItem.length) {
+      sliderItem[i - 1].classList.remove("info-slider__item_active")
+      i = 0
+      sliderItem[i].classList.add("info-slider__item_active")
+   } else {
+      sliderItem[i - 1].classList.remove("info-slider__item_active")
+      sliderItem[i].classList.add("info-slider__item_active")
+   }
+})
+
+
 
 
 
